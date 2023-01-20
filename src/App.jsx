48f75a0be7436ac15c4b86fcce0142ee6ff8d4a7
brunoFernandes21 <47353649+brunoFernandes21@users.noreply.github.com>
2023-01-20 22:14:58 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Meals App</h1>
+    </div>
+  )
+}
+
+export default App
