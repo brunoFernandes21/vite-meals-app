44c4c4db533f0c5159e91df890705d4186267aa7
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-10-20 14:44:30 +0100

    implement favourites functionality and styling

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "../context/Context";
 
 const Favorites = () => {
-  return (
-    <div>
-      <h1>Favorites</h1>
-    </div>
-  )
-}
+  const { removeFromFavourites, selectMeal, favourites } = useGlobalContext();
 
-export default Favorites
+  return (
+    <section className="favourites">
+      <div className="favourites__content">
+        <h5>Favourites</h5>
+        <div className="favourites__container">
+          {favourites.map((meal) => {
+            const { idMeal, strMealThumb: image } = meal;
+            return (
+              <div key={idMeal} className="favourite__meal">
+                <img src={image} alt="" className="favourites__img img" onClick={() => selectMeal(idMeal, true)} />
+                <button
+                  className="remove__btn"
+                  onClick={() => removeFromFavourites(idMeal)}
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Favorites;
